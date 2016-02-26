1b275fcffa60887301b7924dce08b97146ae6345
Jordan Dalton <jordan.8474@gmail.com>
2016-02-26 20:15:56 +0000

    Meh
    This will do.

--- commands/pulldeploy.js
+++ commands/pulldeploy.js
@@ -1,31 +1,30 @@
 ﻿var pullDeployCommand = {
     description: "Fetch code from github and stop the bot",
     process: function (bot, msg, args) {
-        bot.sendMessage(msg.channel, "Updating..", function (err, sentMsg) {
-            console.log("Updating...");
-            var spawn = require("child_process").spawn;
-            var fetch = spawn("git", ["fetch", "origin", "origin/master"]);
+      bot.sendMessage(msg.channel,"Updating...",function(error,sentMsg){
+            console.log("updating...");
+            var spawn = require('child_process').spawn;
 
-            fetch.stdout.on("data", function (data) {
-                console.log("Recived: " + data);
+            var fetch = spawn('git', ['fetch']);
+            fetch.stdout.on('data',function(data){
+                console.log(data.toString());
             });
 
-            fetch.on("close", function (code) {
-                var reset = spawn('git', ['rebase', 'origin/master']); //Merge local with origin
-                console.log("Restarting..");
-
-                reset.stdout.on('data', function (data) {
-                    console.log("checkout Recieved:" + data.toString());
+            fetch.on("close",function(code){
+                var reset = spawn('git', ['reset','--hard','origin/master']);
+                reset.stdout.on('data',function(data){
+                    console.log(data.toString());
+                });
+                reset.on("close",function(code){
+                    console.log("goodbye");
+                    bot.updateMessage(sentMsg,"Brb!",function(){
+                      bot.logout(function(){
+                        process.exit();
+                      });
+                    });
                 });
 
-                reset.on("close", function (code) {
-                    bot.updateMessage(sentMsg, "Shutting down... I've been updated");
-                    bot.logout(function (){
-                        process.exit(1);
-                    })
-                });
             });
-
         });
     }
 };
