84b4050a8c73a4fe5a3948dcff19eeee61f74682
Jordan Dalton <jordan.8474@gmail.com>
2016-02-26 19:49:47 +0000

    Better description for pulldeploy

--- commands/pulldeploy.js
+++ commands/pulldeploy.js
@@ -1,10 +1,11 @@
 ﻿var pullDeployCommand = {
-    description: "Fetch code from github and restart the bot",
+    description: "Fetch code from github and stop the bot",
     process: function (bot, msg, args) {
         bot.sendMessage(msg.channel, "Updating..", function (err, sentMsg) {
             console.log("Updating...");
             var spawn = require("child_process").spawn;
             var fetch = spawn("git", ["fetch", "origin", "origin/master"]);
+
             fetch.stdout.on("data", function (data) {
                 console.log("Recived: " + data);
             });
