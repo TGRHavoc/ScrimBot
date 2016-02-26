a095833d0d3779d98cde2fa4712e8255e62cc6f3
Andrew <andrew.pheasey@gmail.com>
2016-02-26 20:53:03 +0000

    Add `Echo`, `Kys` and `Ping`.

---
+++ commands/echo.js
@@ -0,0 +1,15 @@
+﻿var echoCommand = {
+  usage: "",
+  description: "Echo echo echo..",
+  process: function (bot, msg, arguments) {
+    var roles = bot.servers[0].rolesOf(msg.author);
+    for (var role in roles) {
+      if (role = "Admins") {
+        bot.sendMessage(msg.channel, arguments, function(err, channel) { if(err) console.log(err); });
+        return;
+      }
+    }
+  }
+};
+
+module.exports = echoCommand;
---
+++ commands/kys.js
@@ -0,0 +1,17 @@
+﻿var kysCommand = {
+  usage: "",
+  description: "Quick and easy abbreviation for kill youself.",
+  process: function (bot, msg, arguments) {
+    var roles = bot.servers[0].rolesOf(msg.author);
+    for (var role in roles) {
+      if (role = "Admins") {
+        bot.sendMessage(msg.channel, ":frowning: kys :skull:", function(err, channel) { if(err) console.log(err); });
+        bot.logout(function(err, channel) { if(err) console.log(err); });
+        return;
+      }
+    }
+    bot.sendMessage(msg.channel, "kys " + msg.author.name + " :skull:", function(err, channel) { if(err) console.log(err); });
+  }
+};
+
+module.exports = kysCommand;
