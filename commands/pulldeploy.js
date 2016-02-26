2d46755e354a1dd74f6a27e5bef9bd8e2ba14cf1
Jordan Dalton <jordan.8474@gmail.com>
2016-02-26 18:53:38 +0000

    PullDeploy command

---
+++ commands/pulldeploy.js
@@ -0,0 +1,37 @@
+﻿var pullDeployCommand = {
+    description: "Fetch code from github and restart the bot",
+    process: function (bot, msg, args) {
+        bot.sendMessage(msg.channel, "Updating..", function (err, sentMsg) {
+            console.log("Updating...");
+            var spawn = require("child_process").spawn;
+            var fetch = spawn("git", ["fetch"]);
+            fetch.stdout.on("data", function (data) {
+                console.log("Recived: " + data);
+            });
+
+            fetch.on("close", function (code) {
+                var reset = spawn('git', ['merge', 'origin/master']); //Merge local with origin
+                reset.stdout.on('data', function (data) {
+                    console.log(data.toString());
+                });
+                reset.on("close", function (code) {
+                    var npm = spawn('npm', ['install']);
+                    npm.stdout.on('data', function (data) {
+                        console.log(data.toString());
+                    });
+                    npm.on("close", function (code) {
+                        console.log("goodbye");
+                        bot.sendMessage(msg.channel, "brb!", function () {
+                            bot.logout(function () {
+                                process.exit();
+                            });
+                        });
+                    });
+                });
+            });
+
+        });
+    }
+};
+
+module.exports = pullDeployCommand;
