be5ba898352d605b8ef30cca042500a80f389456
Jordan Dalton <jordan.8474@gmail.com>
2016-03-05 01:59:25 +0000

    Better play command
    Handled some errors...
    
    Added the playing command

---
+++ commands/fun/playing.js
@@ -0,0 +1,13 @@
+var PlayingCommand = {
+	description: "Show the song the bot is currently playing",
+	process: function(bot, msg, args){
+		if(typeof currentlyPlayingSong == "undefined" || !currentlyPlayingSong){
+			bot.sendMessage(msg, `I'm not currently playing a song.`);
+			return;
+		}
+
+		bot.sendMessage(msg, `Now Playing:\n${currentlyPlayingSong.prettyPrint()}`);
+	}
+};
+
+module.exports = PlayingCommand;
