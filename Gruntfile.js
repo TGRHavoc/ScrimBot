8b728a9709d0fb43fad197121e6e066409e8e1b9
Jordan Dalton <jordan.8474@gmail.com>
2016-03-06 01:02:47 +0000

    Travis integration
    Hello Mr Travis

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+	  	uglify: {
+	    	options: {
+	      		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+						        '<%= grunt.template.today("yyyy-mm-dd") %>\\n' +
+						        '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+						        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+						        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+	    	},
+			minify: {
+				files: grunt.file.expandMapping(['lib/**/*.js', 'app.js'], 'destination/', {rename: function(destBase, destPath) { return destBase+destPath.replace('.js', '.min.js'); }
+				})
+			}
+	  	}
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Default task(s).
+	grunt.registerTask('default', ['uglify']);
+};
